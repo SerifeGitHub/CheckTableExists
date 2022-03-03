7aa396cd26f82db8b6ee91254eb04493c5605216
SerifeGitHub <serife.arif@gmail.com>
2022-03-03 22:01:22 +0000

    First commit

---
+++ CreateNewTableAndTransferData.sql
@@ -0,0 +1,12 @@
+
+/* Batch run for checking if database table does not exist and if it doesn't 
+it creates a new table and transfers data from another table into the newly created one */
+
+IF OBJECT_ID ('[JobSatisfaction].[dbo].[ExtremeJobSatisfaction]') IS NULL
+BEGIN
+CREATE TABLE [JobSatisfaction].[dbo].[ExtremeJobSatisfaction] (Employment varchar(255), Employment1 int, JobSatisfaction varchar(255), JobSatisfaction1 int)
+END
+IF OBJECT_ID ('[JobSatisfaction].[dbo].[ExtremeJobSatisfaction]') IS NOT NULL
+INSERT INTO [JobSatisfaction].[dbo].[ExtremeJobSatisfaction] SELECT * FROM [JobSatisfaction].[dbo].[Sheet1$]
+
+SELECT MIN(JobSatisfaction1), MAX(JobSatisfaction1), AVG(JobSatisfaction1) FROM ExtremeJobSatisfaction
